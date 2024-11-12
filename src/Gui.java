6115dce79520ce3c4cdc0786ba0335d3360fb873
Elliot <elliot.nykvist@yh.nackademin.se>
2024-11-12 19:54:37 +0100

    created the JFrame for the gui

---
+++ src/Gui.java
@@ -0,0 +1,13 @@
+import javax.swing.*;
+
+public class Gui extends JFrame {
+
+ public Gui() {
+    this.setSize(300, 300);
+    this.setLocationRelativeTo(null);
+    this.setVisible(true);
+    this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+ }
+
+
+}

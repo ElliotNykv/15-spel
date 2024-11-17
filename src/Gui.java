0ca8f8ee9d8651538b638a38b5bd26d23dc425d6
Elliot <elliot.nykvist@yh.nackademin.se>
2024-11-17 20:45:11 +0100

    moved code from gui to a method called createGameBoard and updated gameMovement method. Also create checkGameOrder

--- src/Gui.java
+++ src/Gui.java
@@ -1,6 +1,7 @@
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
+import java.util.Collections;
 import java.util.List;
 import java.util.ArrayList;
 
@@ -8,7 +9,6 @@ public class Gui extends JFrame {
   List <JButton> buttonList = createButtons();
   private static final int gridSize = 4;
   private static final int allBtns = gridSize * gridSize;
-  private boolean gameWon = false;
 
   JButton newGameBtn = new JButton("Nytt spel");
   JLabel winText = new JLabel("Grattis, du vann!");
@@ -20,15 +20,32 @@ public class Gui extends JFrame {
       if (i == allBtns - 1) {
         JButton button = new JButton();
         buttonList.add(button);
+        button.addActionListener(this::gameMovement);
       } else {
         JButton button = new JButton(String.valueOf(i + 1));
         buttonList.add(button);
-        button.addActionListener(e -> gameMovement(e));
+        button.addActionListener(this::gameMovement);
 
       }
     }
     return buttonList;
   }
+
+  public JPanel createGameBoard() {
+    JPanel gamePanel = new JPanel(new GridLayout(4, 4));
+    List<JButton> buttonListCopy = new ArrayList<>(buttonList);
+    if (buttonListCopy.size() > 1) {
+      Collections.shuffle(buttonListCopy.subList(0, buttonListCopy.size() - 1));
+    }
+
+    for (JButton button : buttonList) {
+      button.setPreferredSize(new Dimension(100, 100));
+      gamePanel.add(button);
+    }
+    return gamePanel;
+
+  }
+
 
   private void gameMovement(ActionEvent e) {
 
@@ -44,24 +61,40 @@ public class Gui extends JFrame {
     }
 
     if ((clickedIndex == emptyIndex - 1) || (clickedIndex == emptyIndex + 1) ||
-            (clickedIndex == emptyIndex - 4) || (clickedIndex == emptyIndex + 4)) {
+            (clickedIndex == emptyIndex - gridSize) || (clickedIndex == emptyIndex + gridSize)) {
+
       buttonList.get(emptyIndex).setText(clickedButton.getText());
       clickedButton.setText("");
+      checkGameOrder();
     }
   }
 
+  private void checkGameOrder() {
+    winText.setVisible(false);
+    for (int i = 0; i < buttonList.size() - 1; i++) {
+      String buttonText = buttonList.get(i).getText();
+      if (Integer.parseInt(buttonText) < i - 1) {
+        return;
+      }
+    }
+
+    checkGameWon();
+  }
+
+  public void checkGameWon() {
+    winText.setVisible(true);
+  }
+
+  
 
 
 
   public Gui() {
+
     this.setLayout(new BorderLayout());
-    JPanel gamePanel = new JPanel(new GridLayout(4, 4));
-    for (JButton button : buttonList) {
-      button.setPreferredSize(new Dimension(100, 100));
-      gamePanel.add(button);
-    }
 
     JPanel EastPanel = new JPanel();
+    JPanel gamePanel = createGameBoard();
     EastPanel.setLayout(new BoxLayout(EastPanel, BoxLayout.Y_AXIS));
     newGameBtn.setAlignmentY(Component.CENTER_ALIGNMENT);
 
